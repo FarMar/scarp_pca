68e2cff4073f619fbf5bee8bcf22cdd7cd594ed8
Mark Farrell <mark.farrell@csiro.au>
2020-09-25 19:11:02 +0930

    tidied up

--- scripts/pca_DON360.R
+++ scripts/pca_DON360.R
@@ -23,7 +23,7 @@ z.fn <- function(x) {
 proc_s <- proc %>% mutate(across(8:1408, z.fn))
 head(proc_s)
 
-
+#PCA
 autoplot(prcomp(proc[8:1408]), data = proc, size = 4, colour = 'LandUse', loadings = FALSE, loadings.label = FALSE)
 autoplot(prcomp(proc_s[8:1408]), data = proc, size = 4, colour = 'LandUse', loadings = FALSE, loadings.label = FALSE)
 
@@ -33,31 +33,40 @@ pc2model <- prcomp(proc[8:1408])
 plot(pc2model$x, col = rgb(0, 0, 0, 0.3), pch = 19, main = "Kennard-Stone (synthetic)")
 grid()
 
-
+#KS
 ks <- kenStone(proc[8:1408], 20, 'euclid')
 ks
 
 points(pc2model$x[ks$model, ], col = "red", pch = 19, cex = 1.4)
 
 #standardised
-pc2smodel <- prcomp(proc_s[8:1408])
+#pc2smodel <- prcomp(proc_s[8:1408])
 
-plot(pc2smodel$x, col = rgb(0, 0, 0, 0.3), pch = 19, main = "Kennard-Stone (synthetic)")
-grid()
+#plot(pc2smodel$x, col = rgb(0, 0, 0, 0.3), pch = 19, main = "Kennard-Stone (synthetic)")
+#grid()
 
 
-ks_s <- kenStone(proc_s[8:1408], 20, 'euclid')
-ks_s
+#ks_s <- kenStone(proc_s[8:1408], 20, 'euclid')
+#ks_s
 
-points(pc2smodel$x[ks_s$model, ], col = "red", pch = 19, cex = 1.4)
+#points(pc2smodel$x[ks_s$model, ], col = "red", pch = 19, cex = 1.4)
 
 
 #outputting
 ##add row ID
 procID <- mutate(proc, ID = row_number())
 
+#add ranks to KS selection
+ks_vec <- as.vector(ks$model)
+rank <- as.vector(1:20)
+ks_out <- tibble(ID = ks_vec, KS_rank = rank)
+
+#filter full df by KS selection
 ks_df <- procID %>% 
-  filter(ID %in% ks$model)
+  filter(ID %in% ks_out$ID)
 
+#Add in KS ranks for easy sorting
+ks_df <- inner_join(ks_out, ks_df)
 
+#output .csv
 write_csv(ks_df, "ks20.csv")

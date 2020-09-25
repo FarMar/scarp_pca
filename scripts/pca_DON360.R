eebf5f5b6061916492c7836d7dfe02047e5fcd01
Mark Farrell <mark.farrell@csiro.au>
2020-09-25 18:17:00 +0930

    plotted

--- scripts/pca_DON360.R
+++ scripts/pca_DON360.R
@@ -24,10 +24,34 @@ proc_s <- proc %>% mutate(across(8:1408, z.fn))
 head(proc_s)
 
 
-autoplot(prcomp(proc[8:1408]), data = proc, size = 4, loadings = FALSE, loadings.label = FALSE)
+autoplot(prcomp(proc[8:1408]), data = proc, size = 4, colour = 'LandUse', loadings = FALSE, loadings.label = FALSE)
+autoplot(prcomp(proc_s[8:1408]), data = proc, size = 4, colour = 'LandUse', loadings = FALSE, loadings.label = FALSE)
 
-ks <- kenStone(proc[2:8], 250, 'euclid')
+
+pc2model <- prcomp(proc[8:1408])
+
+plot(pc2model$x, col = rgb(0, 0, 0, 0.3), pch = 19, main = "Kennard-Stone (synthetic)")
+grid()
+
+
+ks <- kenStone(proc[8:1408], 10, 'euclid')
 ks
-ks2<-as_data_frame(ks[["model"]])
+
+points(pc2model$x[ks$model, ], col = "red", pch = 19, cex = 1.4)
+
+#standardised
+pc2smodel <- prcomp(proc_s[8:1408])
+
+plot(pc2smodel$x, col = rgb(0, 0, 0, 0.3), pch = 19, main = "Kennard-Stone (synthetic)")
+grid()
+
+
+ks_s <- kenStone(proc_s[8:1408], 10, 'euclid')
+ks_s
+
+points(pc2model$x[ks_s$model, ], col = "red", pch = 19, cex = 1.4)
+### - so don't standardise!
+
+ks_df<-as_tibble(ks[["model"]])
 
 write_csv(ks2, "data/working/ks250.csv")
